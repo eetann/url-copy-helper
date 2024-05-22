f245d40ae8a0d982df47540412776ad5e57cc147
eetann <hideharu092@gmail.com>
2024-05-23 08:31:38 +0900

    style: Tailwindの導入

--- src/entrypoints/popup/main.ts
+++ src/entrypoints/popup/main.ts
@@ -1,4 +1,4 @@
-import './app.css';
+import '$lib/app.css';
 import App from './App.svelte';
 
 const app = new App({
--- wxt.config.ts
+++ wxt.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'wxt';
 import { svelte, vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import path from "path";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   srcDir: 'src',
   vite: () => ({
+    resolve: {
+      alias: {
+        $lib: path.resolve("./src/lib"),
+      },
+    },
     plugins: [
       svelte({
         // Using a svelte.config.js file causes a segmentation fault when importing the file
