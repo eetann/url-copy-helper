e2208bd31974288b915de00a587fdec39782e75b
eetann <hideharu092@gmail.com>
2024-05-23 08:59:04 +0900

    feat: サンプルのリストを表示

--- src/entrypoints/popup/App.svelte
+++ src/entrypoints/popup/App.svelte
@@ -1,20 +1,23 @@
 <script lang="ts">
-  import Counter from "$lib/Counter.svelte";
+  let textList = [
+    { name: "Markdown", text: "[title](URL)" },
+    { name: "URL only", text: "URL" },
+    { name: "HTML", text: "<a href='URL'>title</a>" },
+  ];
 </script>
 
-<main class="min-w-96 min-h-96">
-  <h1>WXT + Svelte</h1>
-  <p class="bg-red-600">aaaaaaaaaaaaa</p>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p class="read-the-docs">Click on the WXT and Svelte logos to learn more</p>
+<main class="min-w-96 min-h-96 prose">
+  <ul class="menu bg-base-200">
+    {#each textList as item}
+      <li>
+        <div class="flex flex-col items-start">
+          <div>{item.name}</div>
+          <div>{item.text}</div>
+        </div>
+      </li>
+    {/each}
+  </ul>
 </main>
 
 <style>
-  .read-the-docs {
-    color: #888;
-  }
 </style>
