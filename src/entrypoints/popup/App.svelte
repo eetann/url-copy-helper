d2939c31d8d8432585fe4b8cd2421696f56044ac
eetann <hideharu092@gmail.com>
2024-05-26 10:27:25 +0900

    feat: 実際に現在のタブから文字列を生成

--- src/entrypoints/popup/App.svelte
+++ src/entrypoints/popup/App.svelte
@@ -1,66 +1,77 @@
 <script lang="ts">
   import Icon from "@iconify/svelte";
-  import { cn } from "$lib/utils";
-  let notice: number | null = null;
+  import { cn, CopyUrlHelper } from "$lib/utils";
+  import type { TextDict } from "$lib/utils";
+  let notice: number | undefined = undefined;
+  let textDict: TextDict | undefined = undefined;
 
-  const url = "https://example";
-  const title = "TITLE HERE";
-  const richText = `<a href="${url}">${title}</a>`;
-  let textList = [
-    { name: "Markdown", text: "[title](URL)" },
-    { name: "URL only", text: "URL" },
-    { name: "HTML", text: "<a href='URL'>title</a>" },
-  ];
-  async function copyText(text: string, index: number) {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    const currentTab = tabs[0];
+    const title = currentTab.title ?? "No Titile";
+    const url = currentTab.url ?? "No URL";
+    const helper = new CopyUrlHelper(title, url);
+    textDict = helper.getTextDict();
+  });
+
+  async function copyText(key: string, index: number) {
+    if (!textDict) {
+      return;
+    }
+    const text = textDict[key].text;
     const item = [
       new window.ClipboardItem({
-        "text/html": new Blob([richText], { type: "text/html" }),
+        "text/html": new Blob([textDict.html.text], { type: "text/html" }),
         "text/plain": new Blob([text], { type: "text/plain" }),
       }),
     ];
     await navigator.clipboard.write(item);
     notice = index;
     setTimeout(() => {
-      notice = null;
+      notice = undefined;
     }, 3000);
   }
 </script>
 
 <main class="min-w-96 min-h-96 prose">
   <ul class="menu menu-sm">
-    {#each textList as item, index}
-      <li>
-        <!-- svelte-ignore a11y-autofocus -->
-        <button
-          type="submit"
-          tabindex={index}
-          autofocus={index === 0 ? true : false}
-          on:click={() => {
-            copyText(item.text, index);
-          }}
-          class="flex justify-between btn"
-        >
-          <div class="flex flex-col items-start">
-            <div class="font-bold">{item.name}</div>
-            <div>{item.text}</div>
-          </div>
-          <span class={cn("swap", notice === index ? "swap-active" : "")}>
-            <Icon
-              class="swap-off"
-              icon="heroicons-outline:clipboard-copy"
-              width="28"
-              height="28"
-            />
-            <Icon
-              class="swap-on"
-              icon="heroicons-outline:clipboard-check"
-              width="28"
-              height="28"
-            />
-          </span>
-        </button>
-      </li>
-    {/each}
+    {#if textDict != undefined}
+      {#each Object.keys(textDict) as key, index}
+        <li>
+          <!-- svelte-ignore a11y-autofocus -->
+          <button
+            type="submit"
+            tabindex={index}
+            autofocus={index === 0 ? true : false}
+            on:click={() => {
+              if (!textDict) {
+                return;
+              }
+              copyText(key, index);
+            }}
+            class="flex justify-between btn"
+          >
+            <div class="flex flex-col items-start">
+              <div class="font-bold">{textDict[key].name}</div>
+              <div>{textDict[key].format}</div>
+            </div>
+            <span class={cn("swap", notice === index ? "swap-active" : "")}>
+              <Icon
+                class="swap-off"
+                icon="heroicons-outline:clipboard-copy"
+                width="28"
+                height="28"
+              />
+              <Icon
+                class="swap-on"
+                icon="heroicons-outline:clipboard-check"
+                width="28"
+                height="28"
+              />
+            </span>
+          </button>
+        </li>
+      {/each}
+    {/if}
   </ul>
 </main>
 
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,59 @@ import { twMerge } from "tailwind-merge";
 export function cn(...args: ClassValue[]) {
   return twMerge(clsx(args));
 }
+
+export type TextDict = Record<string, {
+  name: string;
+  format: string;
+  text: string;
+}>;
+
+export class CopyUrlHelper {
+  private title;
+  private url;
+  private githubOnlyRepo = true;
+  constructor(title: string, url: string, githubOnlyRepo?: boolean) {
+    this.title = title
+    if (this.githubOnlyRepo && /github.com\/[^\/]*\/[^\/]*$/.test(url)) {
+      const capture = /\/(.*):.*$/.exec(title);
+      if (capture && capture[1]) {
+        this.title = capture[1];
+      }
+    }
+    this.url = url;
+    if (githubOnlyRepo != undefined) {
+      // TODO: エンコード？
+      this.githubOnlyRepo = githubOnlyRepo;
+    }
+  }
+
+  getUrl() {
+    return this.url;
+  }
+
+  getTitle() {
+    return this.title;
+  }
+
+  getHTML() {
+    return `<a href="${this.url}">${this.title}</a>`;
+  }
+
+  getMarkdown() {
+    return `[${this.title}](${this.url})`
+  }
+
+  getWikiLink() {
+    return `[[${this.url}|${this.title}]]`
+  }
+
+  getTextDict(): TextDict {
+    return {
+      markdown: { name: "Markdown", format: "[title](URL)", text: this.getMarkdown() },
+      html: { name: "HTML", format: "<a href='URL'>title</a>", text: this.getHTML() },
+      url: { name: "URL", format: "URL", text: this.getUrl() },
+      title: { name: "Title", format: "title", text: this.getTitle() },
+      wikiLink: { name: "wikiLink", format: "[[URL|title]]", text: this.getWikiLink() },
+    };
+  }
+}
