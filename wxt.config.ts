32663d0fa0f88f18623c30f7f02c325cb9486645
eetann <hideharu092@gmail.com>
2024-07-06 16:11:12 +0900

    chore: バージョンアップ

--- wxt.config.ts
+++ wxt.config.ts
@@ -4,53 +4,53 @@ import path from "node:path";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-  manifest: {
-    name: "URL Copy Helper",
-    version: "1.0.1",
-    description: "Copy current URL and Title. Rich Text is avalable!",
-    permissions: ["tabs", "contextMenus", "activeTab", "scripting"],
-    action: {
-      default_popup: "popup.html",
-      default_icon: {
-        "32": "icon/32.png",
-        "48": "icon/48.png",
-        "96": "icon/96.png",
-        "128": "icon/128.png",
-      },
-    },
-    commands: {
-      // popup: {
-      // 	description: "Open popup view",
-      // },
-      Markdown: {
-        description: "Copy in Markdown format",
-      },
-      HTML: {
-        description: "Copy in HTML format",
-      },
-      URL: {
-        description: "Copy in URL format",
-      },
-      Title: {
-        description: "Copy in Title format",
-      },
-      WikiLink: {
-        description: "Copy in WikiLink format",
-      },
-    },
-  },
-  srcDir: "src",
-  vite: () => ({
-    resolve: {
-      alias: {
-        $lib: path.resolve("./src/lib"),
-      },
-    },
-    plugins: [
-      svelte({
-        // Using a svelte.config.js file causes a segmentation fault when importing the file
-        configFile: path.resolve("./svelte.config.mjs"),
-      }),
-    ],
-  }),
+	manifest: {
+		name: "URL Copy Helper",
+		version: "1.0.2",
+		description: "Copy current URL and Title. Rich Text is avalable!",
+		permissions: ["tabs", "contextMenus", "activeTab", "scripting"],
+		action: {
+			default_popup: "popup.html",
+			default_icon: {
+				"32": "icon/32.png",
+				"48": "icon/48.png",
+				"96": "icon/96.png",
+				"128": "icon/128.png",
+			},
+		},
+		commands: {
+			// popup: {
+			// 	description: "Open popup view",
+			// },
+			Markdown: {
+				description: "Copy in Markdown format",
+			},
+			HTML: {
+				description: "Copy in HTML format",
+			},
+			URL: {
+				description: "Copy in URL format",
+			},
+			Title: {
+				description: "Copy in Title format",
+			},
+			WikiLink: {
+				description: "Copy in WikiLink format",
+			},
+		},
+	},
+	srcDir: "src",
+	vite: () => ({
+		resolve: {
+			alias: {
+				$lib: path.resolve("./src/lib"),
+			},
+		},
+		plugins: [
+			svelte({
+				// Using a svelte.config.js file causes a segmentation fault when importing the file
+				configFile: path.resolve("./svelte.config.mjs"),
+			}),
+		],
+	}),
 });
